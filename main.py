c675c173d8136c09e6532da39f82836e91fb2559
John Adeleke <89088298+gostlite@users.noreply.github.com>
2022-07-23 12:55:04 +0100

    Update main.py
    j

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 def csv_split(filename, user_specified_size):
     accepted_fmt = ["json", "csv"]
     doc_name, extension = filename.split(".")[0], filename.split(".")[1]
-    print(extension)
     """checking if file is an accepted fmt"""
     if extension not in accepted_fmt:
         raise TypeError("Sorry input a an accepted format json or csv file")
@@ -10,7 +9,6 @@ def csv_split(filename, user_specified_size):
         chunk_file_size = math.ceil(size_of_file / user_specified_size)
         total_lenght_of_file = len(pd.read_csv(filename).index)
         row_per_file = math.ceil(total_lenght_of_file / chunk_file_size)
-        print(row_per_file)
         num = 1
         for each_file in pd.read_csv(filename, chunksize=row_per_file, header=None):
             each_file.to_csv(f"{doc_name}{num}.csv")
